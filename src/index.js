5e514aea2ccaa66827e5c8c66797927eedde9d0b
Tanner Nielsen <tannerntannern@gmail.com>
2019-06-22 09:30:28 -0500

    Added some default code

--- src/index.js
+++ src/index.js
@@ -30,7 +30,25 @@ self.MonacoEnvironment = {
 // window.monaco = monaco;
 
 window.monacoEditor = monaco.editor.create(document.getElementById('monaco-editor'), {
-	value: '',
+	value:
+`// see https://github.com/tannerntannern/budgeteer for help!
+// (the highlighting errors will be fixed soon)
+
+const wages = supply('Wages', 2500);
+const checking = pipe('Checking');
+const expenses = pipe('Expenses');
+
+wages
+	.supplies(700).to(consumer('Taxes'))
+	.supplies(1200).to(checking)
+	.suppliesAsMuchAsPossible().to(consumer('Savings'));
+
+checking
+	.suppliesAsMuchAsNecessary().to(expenses)
+	.suppliesAsMuchAsPossible().to(consumer('Spending Money'));
+
+consumer('Rent').consumes(900).from(expenses);
+consumer('Groceries').consumes(200).from(expenses);`,
 	language: 'typescript'
 });
 
@@ -38,10 +56,16 @@ window.processBudgeteer = function () {
 	reset();
 	eval(window.monacoEditor.getValue());
 
-	let transfers = solve().transfers;
-	let sankeymaticString = '';
-	transfers.forEach((node1, node2, value) => sankeymaticString += `${node1.name} [${value.toFixed(2)}] ${node2.name}\n`);
+	try {
+		let transfers = solve().transfers;
+		let sankeymaticString = '';
+		transfers.forEach((node1, node2, value) => sankeymaticString += `${node1.name} [${value.toFixed(2)}] ${node2.name}\n`);
 
-	document.getElementById('flows_in').value = sankeymaticString;
-	window.process_sankey();
+		document.getElementById('flows_in').value = sankeymaticString;
+		window.process_sankey();
+	} catch (e) {
+		alert(e.message);
+	}
 };
+
+window.processBudgeteer();
